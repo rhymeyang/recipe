1b2199e1bcbb2f683055146eb84039634eaca12c
rhyme_yang <rhyme_yang@live.cn>
2024-09-24 21:31:04 -0400

    init deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,47 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  base: '/recipe/',
+  theme: defaultTheme({
+    logo: '/recipe/images/logo.png',
+    navbar: false,
+    sidebar: {
+      '/': [
+        {
+          text: 'Main Pages',
+          children: [
+            '/',
+          ],
+        },
+        {
+          text: '中餐',  // Another section title
+          collapsable: true,         // This section is collapsible
+          children: [
+            '/Dish/pork.md',
+            '/Dish/eggs.md',
+          ],
+        },
+        {
+          text: '面点',
+          collapsable: true,
+          children: [
+            '/Pastry/cake.md',
+            '/Pastry/dimsun.md',
+          ],
+        },
+        {
+          text: '调料',
+          collapsable: true,
+          children: [
+            '/Ingredient/powder.md',
+          ],
+        }
+      ]
+    },
+  }),
+  lang: 'zh-CN',
+  title: "Vicky's Recipe",
+  description: 'Recipe Collection',
 })
 
 
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,1 @@
-# Hello VuePress
+# Vicky's Recipe Collection
